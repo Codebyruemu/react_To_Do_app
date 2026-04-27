2e5b679b135fcc9901c1348f83da8c0992c0830f
codebyruemu <reforuemu@gmail.com>
2026-04-27 01:30:28 +0100

    fift commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react_To_Do_app/',
+  // base: '/react_To_Do_app/',
+  base: '/',
 })
